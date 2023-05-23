7c75f40775cd73f97bce8b4d32725cf9734164dc
Corey Rynvis <corfoto4@outlook.com>
2023-05-23 22:30:43 +0800

    Initial commit

---
+++ src/main/java/net/corfoto4/voicechatbroadcastmod/VoicechatBroadcastMod.java
@@ -0,0 +1,14 @@
+package net.corfoto4.voicechatbroadcastmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class VoicechatBroadcastMod implements ModInitializer {
+	public static final String MOD_ID = "voicechatbroadcastmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/corfoto4/voicechatbroadcastmod/VoicechatBroadcastModClient.java
@@ -0,0 +1,10 @@
+package net.corfoto4.voicechatbroadcastmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VoicechatBroadcastModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
