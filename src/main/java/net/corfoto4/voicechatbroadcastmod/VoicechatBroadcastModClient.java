b7475ca8b406faa8f8a322aa4d9503b27bd1b6ea
Corey Rynvis <corfoto4@outlook.com>
2023-05-23 23:34:59 +0800

    Add voicechat API to project

--- src/main/java/net/corfoto4/voicechatbroadcastmod/VoicechatBroadcastModClient.java
+++ src/main/java/net/corfoto4/voicechatbroadcastmod/VoicechatBroadcastModClient.java
@@ -1,6 +1,7 @@
 package net.corfoto4.voicechatbroadcastmod;
 
 import net.fabricmc.api.ClientModInitializer;
+//import de.maxhenkel.voicechat.api.VoicechatPlugin;
 
 public class VoicechatBroadcastModClient implements ClientModInitializer {
     @Override
