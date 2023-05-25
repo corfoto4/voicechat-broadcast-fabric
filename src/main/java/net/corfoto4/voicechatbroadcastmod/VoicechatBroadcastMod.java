3c54fc1141720f65031c9d1cbf8f6d0df8bab0b0
Corey Rynvis <corfoto4@outlook.com>
2023-05-25 19:27:29 +0800

    cleaned up code

--- src/main/java/net/corfoto4/voicechatbroadcastmod/VoicechatBroadcastMod.java
+++ src/main/java/net/corfoto4/voicechatbroadcastmod/VoicechatBroadcastMod.java
@@ -1,7 +1,6 @@
 package net.corfoto4.voicechatbroadcastmod;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.loader.api.FabricLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
