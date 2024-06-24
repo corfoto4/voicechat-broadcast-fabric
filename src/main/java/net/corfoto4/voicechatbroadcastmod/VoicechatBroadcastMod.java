14f8cdff6cfb0e8aa8664a2406e4ea9f2bca6ca3
Corey Rynvis <corfoto4@outlook.com>
2024-06-24 10:58:12 +0800

    fixed versions, cleaned configs

--- src/main/java/net/corfoto4/voicechatbroadcastmod/VoicechatBroadcastMod.java
+++ src/main/java/net/corfoto4/voicechatbroadcastmod/VoicechatBroadcastMod.java
@@ -8,7 +8,7 @@ import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.minecraft.server.MinecraftServer;
 
 public class VoicechatBroadcastMod implements ModInitializer {
-	public static final String MOD_ID = "voicechatbroadcastmod";
+	public static final String MOD_ID = "voicechatbroadcast";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	public static MinecraftServer minecraftServer;
 
@@ -18,6 +18,6 @@ public class VoicechatBroadcastMod implements ModInitializer {
 		ServerLifecycleEvents.SERVER_STARTED.register(server -> VoicechatBroadcastMod.minecraftServer = server);
 
 		// Provide log to say the mod is initialized
-		LOGGER.info("Broadcast Mod Initialized");
+		LOGGER.info("Voicechat Broadcast Initialised");
 	}
 }
